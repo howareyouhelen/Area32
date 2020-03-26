f792f0f6170de92a8b6627945ff62883e9ebc0c5
Tegvaran Sooch <tegvaran328@gmail.com>
2020-03-25 18:56:11 -0700

    trying to make cards dynamically

--- Script/eventCards.js
+++ Script/eventCards.js
@@ -1,55 +1,156 @@
-
-/**
- * Represents the event object. It stores the specified values of an event.
- * @param {*} type the type of event 
- * @param {*} name  name of the event as a string
- * @param {*} date  the date it is being held at.
- * @param {*} price  the price of teh event
- * @param {*} description description of the event 
- * @param {*} hostName name of the host
- */
-function eventCard(type, name, date, price, description, hostName){
+$(document).ready(function() {
+    // <!----------------------------------Validation and uploading of Event Creation ------------------------------------>
+        var counter = 0;
+        var validImagetypes = ["image/gif", "image/jpeg", "image/png"];
     
-    this.type = type;
-    this.name = name;
-    this.date = date;
-    this.price = price;
-    this.description = description;
-    this.hostName = hostName;
-
-    db.collection("Events").add({
-        type : this.type,
-        name : this.name,
-        date : this.date,
-        price : this.price,
-        description : this.description,
-        hostName : this.hostName
-    })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
+        $("#selected-image").hide();
+    
+        function previewImage(image_event) {
+        if(image_event.files && image_event.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function(e) {
+            $("#selected-image").attr('src', e.target.result);
+            }
+        reader.readAsDataURL(image_event.files[0]);
+        
+        $("#selected-image").show(); 
+        }
+        }
+    
+        $("#main-image").change(function() {
+        previewImage(this);
+        });
+    
+        $("#save-event").click(function(){
+        $("#main-desc").removeClass("is-invalid");
+        $("#main-image").removeClass("is-invalid");
+        $("#main-eventname").removeClass("is-invalid");
+        $("#main-price").removeClass("is-invalid");
+        $("#main-type").removeClass("is-invalid");
+        $("#main-eventdate").removeClass("is-invalid");
+        $("#main-location").removeClass("is-invalid");
+    
+        var desc = $("#main-desc").val();
+        var image = $("#main-image").prop("files")[0];
+        var eventname = $("#main-eventname").val();
+        var price = $("#main-price").val();
+        var type = $("#main-type").val();
+        var eventdate = $("#main-eventdate").val();
+        var location = $("#main-location").val();
+    
+        if(!desc){
+            $("#main-desc").addClass("is-invalid");
+            return;
+        }
+        if(image == null) {
+            $("#main-image").addClass("is-invalid");
+            return;
+        }
+        if($.inArray(image["type"], validImagetypes)<0){
+            $("#main-image").addClass("is-invalid");
+            return;
+        }
+        if(!eventname){
+            $("#main-eventname").addClass("is-invalid");
+        }
+        if(!price){
+            $("#main-price").addClass("is-invalid");
+        }
+        if(!type){
+            $("#main-type").addClass("is-invalid");
+        }
+        if(!eventdate){
+            $("#main-eventdate").addClass("is-invalid");
+        }
+        if(!location){
+            $("#main-location").addClass("is-invalid");
+        }
+    
+    
+    // UPLOAD AND SAVE TO FIREBASE STORAGE AND DATABASE
+        var databaseRef = firebase.database().ref().child("Events");
+    
+        databaseRef.once("value").then(function(){
+            //unique name for storage
+            var name = image["name"];
+            var dateStr = new Date().getTime();
+            var fileCompleteName = name + "_" + dateStr;
+    
+            var storageRef = firebase.storage().ref("Event Images");
+            var eventStorageRef = storageRef.child(fileCompleteName);
+            var uploadTask = eventStorageRef.put(image);
+    
+            uploadTask.on("state_changed",
+    
+                function progress(snapshot){
+                    var percentage = (snapshot.byteTransferred / snapshot.totalBytes) * 100;
+                    $("#upload-progress").html(Math.round(percentage) + "%");
+                    $("#upload-progress").attr("style", "width:" + percentage + "%");
+                },
+                function error(err) {
+                    
+                },
+                function complete() {
+    
+                    var user = firebase.auth().currentUser;
+                    var userName;
+                    firebase.database().ref('users/' + user.uid).once('value').then(function(snapshot){
+                        var name = (snapshot.val() && snapshot.val().name);
+                        var email = (snapshot.val() && snapshot.val().email);
+                        var userName = name + " " + email;
+                    });
+    
+                    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadUrl){
+                        var time = new Date();
+                        var options = {
+                            weekday: "long",
+                            month: "long",
+                            day: "2-digit",
+                            year: "numeric",
+                        };
+    
+                        var eventData = {
+                            "image": downloadUrl,
+                            "name": fileCompleteName,
+                            "desc": desc,
+                            "eventname": eventname,
+                            "price": price,
+                            "type": type,
+                            "eventdate": eventdate,
+                            "location": location,
+                            "uid": user.uid,
+                            "name": userName,
+                            "time": time.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true}),
+                            "date":time.toLocaleString('en-US', options),
+                        };
+                        console.log(databaseRef);
+                        var newEventRef = databaseRef.push();
+                        newEventRef.set(JSON.parse( JSON.stringify(eventData)), function(error){
+                            if(error){
+                                $("#result").attr("class", "alert-danger");
+                                $("#result").html(error.message);
+                            } else {
+                                $("#result").attr("class", "alert-success");
+                                $("#result").html("Event has been created successfully.");
+    
+                                window.open("", "_self");
+                            } 
+                        resetForm();
+                            });
+                        });
+                    }
+                );
+    
+            });
+        });
+        // UPLOAD ENDS HERE
+    // <!----------------------------------Validation and uploading of Event Creation Ends Here ------------------------------------>
+        function resetForm(){
+            $("#main-form")[0].reset();
+            $("#upload-progress").html("Completed");
+        }
+    
+    
+    
     });
-
-    this.getType = function(){
-        return this.type;
-    }
-    this.getName = function(){
-        return this.name;
-    }
-    this.getDate = function(){
-        return this.date;
-    }
-    this.getPrice = function(){
-        return this.price;
-    }
-    this.getDescription = function(){
-        return this.description;
-    }
-    this.getHoseName = function(){
-        return this.hostName;
-    }
-}
-
-let twerkvaran = new eventCard("Dance", "TAN", "2020-04-08", "Free", "Online twerking workshop with Teg.", "TEG");
